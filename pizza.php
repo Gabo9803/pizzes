adcf55c436769838583f6f7d8a337b30bed064d3
Gabo9803 <15585203.clot@fje.edu>
2025-02-11 15:01:51 +0000

    Commit 5 de l'aplicatiu pizzes

--- pizza.php
+++ pizza.php
@@ -8,24 +8,27 @@
  $preu = 8;
  $IVA=21;
  if (empty($_GET['ingr'])) {
- $pvp=((100+$IVA)/100)*$preu;
- echo "Has demanat només la pizza bàsica. El preu és $pvp € (IVA inclòs)<br>";
- exit(0);
+     $pvp=((100+$IVA)/100)*$preu;
+     echo "Has demanat només la pizza bàsica. El preu és $pvp € (IVA inclòs)<br>";
+     # exit(0);
  }
  else {
- $IVA=21;
- $pvp=((100+$IVA)/100)*($preu+count($_GET['ingr']));
- echo "Has demanat la pizza bàsica amb:<br> ";
- foreach ($_GET['ingr'] as $ingredient) {
- if ($ingredient) {
- echo "* $ingredient<br>";
+     $pvp=((100+$IVA)/100)*($preu+count($_GET['ingr']));
+     echo "Has demanat la pizza bàsica amb:<br> ";
+     foreach ($_GET['ingr'] as $ingredient) {
+         if ($ingredient) {
+             echo "* $ingredient<br>";
+         }
+     }
+     echo "El preu és $pvp € (IVA inclòs)<br>";
+     # exit(0);
  }
- }
- echo "El preu és $pvp € (IVA inclòs)<br>";
- exit(0);
-}
-echo "<br>";
-echo "Autor: Gabo9803"."<br>";
+ echo "<br>";
+ echo "Autor: Gabo9803"."<br>";
  ?>
+
+ <!-- Afegim l'enllaç per tornar al formulari -->
+ <a href="pizza.html">Retorna al formulari de selecció d’ingredients</a>
+
  </body>
 </html>
